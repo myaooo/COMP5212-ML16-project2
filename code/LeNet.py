e672d2e99550af40fe5daceaed90b8608aae3c83
Ming Yao <mingyao@mingyaodeMacBook-Pro.local>
2016-10-19 15:02:16 +0800

    init commit

---
+++ code/LeNet.py
@@ -0,0 +1,12 @@
+# ML 2016, HKUST
+# Project 2
+# MING Yao
+# yaoming.thu@gmail.com
+
+
+
+if __name__ == "__main__":
+
+    from tensorflow.examples.tutorials.mnist import input_data
+
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)   
---
+++ report/report.md
@@ -0,0 +1,5 @@
+% Machine Learning 2016 Fall
+  \break Project 2
+% Ming Yao (# 20365468)
+% October 17th, 2016
+
