d56de226bfb3cd8e3d90ff495ff26386bd83cf5d
Ming Yao <mingyao@mingyaodeMacBook-Pro.local>
2016-11-06 13:52:43 +0800

    delete useless

--- remote_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ssh mingyao@vis.cse.ust.hk "cd ~/ml16/COMP5212-ML16-project2/code; git pull; bash ./run.sh"
-scp -r mingyao@vis.cse.ust.hk:~/ml16/COMP5212-ML16-project2/code/output/ ./output
-scp -r mingyao@vis.cse.ust.hk:~/ml16/COMP5212-ML16-project2/code/run.log ./
